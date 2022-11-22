c094e21327f4a43d5fe419b24850f9f87876aff8
tania <taniaosano15@gmail.com>
2022-11-22 22:54:18 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :birds
+  resources :birds, only: [:index, :show]
 end
+
+
+# Line 3- the tool resources enables Rails to create all RESTful routes needed and map them to appropriate controller actions
